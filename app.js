26e4a42a405c4d176bb9f7724e3cdf402ea5d886
Prince Kumar <princekumar007p@gmail.com>
2024-10-21 12:27:50 +0530

    Final commit

--- app.js
+++ app.js
@@ -8,12 +8,30 @@ const {auth} = require('./middleware/authentication')
 const authRoute = require('./routes/auth');
 const jobRoute = require('./routes/jobs') ;
 
+// security packages
+const helmet = require('helmet') ;
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-app.use(express.json());
 // extra packages
+app.set('trust proxy' , 1) ;
+app.use(express.json());
+app.use(cors);
+app.use(xss);
+app.use(rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Redis, Memcached, etc. See below.
+}));
+app.use(helmet);
 
 // routes
 
